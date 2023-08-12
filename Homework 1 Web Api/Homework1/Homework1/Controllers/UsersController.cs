6586f3abc7ceb4c10277cb22f694ac48ace9c520
simon-k1993 <kozoloski_simon@yahoo.com>
2023-08-12 20:27:37 +0200

    homework1 web api

---
+++ Homework 1 Web Api/Homework1/Homework1/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> Get()
+        {
+            return StatusCode(StatusCodes.Status200OK, StaticDb.UserNames);
+
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<string> GetByIndex(int index)
+        {
+            try
+            {
+                if (index < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "The index has negative value!");
+                }
+
+                if (index >= StaticDb.UserNames.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"There is no resource on index {index}");
+                }
+
+                return StatusCode(StatusCodes.Status200OK, StaticDb.UserNames[index]);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred. Contact the administrator.");
+            }
+        }
+    }
+}
---
+++ Homework 1 Web Api/Homework1/Homework1/StaticDb.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework1
+{
+    public static class StaticDb
+    {
+        public static List<string> UserNames = new List<string>
+        {
+            "Toso",
+            "Simon",
+            "Stanko",
+            "Petko",
+            "Mile",
+            "Cacko"
+        };
+    }
+}
